62d39e60cc0c31f4fee3f56c67deb24e7adfd5a3
Ruben Bottu <ruben.bottu@student.ucll.be>
2021-02-16 11:59:28 +0100

    test test test

--- src/main/java/domain/model/User.java
+++ src/main/java/domain/model/User.java
@@ -1,6 +1,6 @@
 package domain.model;
 // This is an epic user
 public class User {
-    //dit gaat er los over [bruh]
+    //dit gaat er los over toch
     String userid,firstName, lastName, password;
 }
