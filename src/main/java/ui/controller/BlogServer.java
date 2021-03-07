ab19945cc4d2cf3424c7367f311106698be91fae
Lennert Van Oosterwyck <lennert.vanoosterwyck13@gmail.com>
2021-03-07 14:57:39 +0100

    probeersel 1c

--- src/main/java/ui/controller/BlogServer.java
+++ src/main/java/ui/controller/BlogServer.java
@@ -1,7 +1,10 @@
 package ui.controller;
 
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,5 +13,17 @@ import java.util.Set;
 @ServerEndpoint("/comment")
 public class BlogServer {
     private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
-    
+
+
+    @OnOpen
+    public void onOpen(Session session) {
+        sessions.add(session);
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session ) throws IOException {
+        for(Session s: sessions) {
+            s.getBasicRemote().sendText(message);
+        }
+    }
 }
